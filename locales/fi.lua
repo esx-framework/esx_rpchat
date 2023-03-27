8de8d3fb40271dede40a1f3189284c9c3c75d8fa
Blooper <122776166+Blo0per@users.noreply.github.com>
2023-03-27 11:41:09 +0300

    Update fi.lua
    ### _Small update for fi.lua_

--- locales/fi.lua
+++ locales/fi.lua
@@ -1,11 +1,11 @@
 Locales['fi'] = {
   ['ooc_prefix'] = 'OOC | %s',
-  ['twt_help'] = 'Lähetä twiitti',
+  ['twt_help'] = 'lähetä twiitti',
   ['twt_prefix'] = '^0[^4Twitter^0] (^5@%s^0)',
-  ['me_help'] = 'Teet jotain',
+  ['me_help'] = 'tee jotain',
   ['me_prefix'] = 'me | %s',
-  ['do_help'] = 'roolipeli tiedot',
+  ['do_help'] = 'tee jotain',
   ['do_prefix'] = 'do | %s',
-  ['generic_argument_name'] = 'Viesti',
-  ['generic_argument_help'] = 'Viestin sisältö',
+  ['generic_argument_name'] = 'viesti',
+  ['generic_argument_help'] = 'viestin sisältö',
 }
