2e85915b061b1d1518fc22fcc2c28d07a080e7eb
DyroS3 <64310374+DyroS3@users.noreply.github.com>
2023-03-27 12:18:09 +0800

    Create sc.lua

---
+++ locales/sc.lua
@@ -0,0 +1,11 @@
+Locales['sc'] = {
+  ['ooc_prefix'] = 'OOC | %s',
+  ['twt_help'] = '发送新推特',
+  ['twt_prefix'] = '^0[^4Twitter^0] (^5@%s^0)',
+  ['me_help'] = '个人消息',
+  ['me_prefix'] = 'me | %s',
+  ['do_help'] = 'RP 详情',
+  ['do_prefix'] = 'do | %s',
+  ['generic_argument_name'] = '文本消息',
+  ['generic_argument_help'] = '文本消息',
+}
