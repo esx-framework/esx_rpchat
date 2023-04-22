318a143218c14ac4aa12212029887907f8cd843e
Lenny822 <84620158+Lenny822@users.noreply.github.com>
2023-04-22 10:01:25 +0200

    Create de.lua

---
+++ locales/de.lua
@@ -0,0 +1,11 @@
+Locales['de'] = {
+  ['ooc_prefix'] = 'OOC | %s',
+  ['twt_help'] = 'Tweet Senden',
+  ['twt_prefix'] = '^0[^4Twitter^0] (^5@%s^0)',
+  ['me_help'] = 'Persönliche Aktionen',
+  ['me_prefix'] = 'Ich | %s',
+  ['do_help'] = 'RP Information',
+  ['do_prefix'] = 'Machen | %s',
+  ['generic_argument_name'] = 'Nachricht',
+  ['generic_argument_help'] = 'Die Nachricht',
+}
