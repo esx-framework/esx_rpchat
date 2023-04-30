4d01d866e83b1bd750fba0295d009c606943880c
SteliosLua <132164877+SteliosLua@users.noreply.github.com>
2023-04-30 09:48:00 +0300

    el.lua

---
+++ locales/el.lua
@@ -0,0 +1,11 @@
+Locales['el'] = {
+    ['ooc_prefix'] = 'Εκτός ρόλου | %s',
+    ['twt_help'] = 'Αποστολή tweet',
+    ['twt_prefix'] = '^0[^4Twitter^0] (^5@%s^0)',
+    ['me_help'] = 'Προσωπική ενέργεια',
+    ['me_prefix'] = 'Εγώ | %s',
+    ['do_help'] = 'Πληροφορίες RP',
+    ['do_prefix'] = 'Κάνω | %s',
+    ['generic_argument_name'] = 'Μήνυμα',
+    ['generic_argument_help'] = 'Το μήνυμα',
+}
