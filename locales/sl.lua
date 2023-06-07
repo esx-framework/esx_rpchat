1d9df1a799604b1402de76a86058cdfff9539e00
C3h1c <fivemsloofficial@gmail.com>
2023-06-07 18:13:19 +0200

    sl.lua

---
+++ locales/sl.lua
@@ -0,0 +1,11 @@
+Locales['sl'] = {
+  ['ooc_prefix'] = 'OOC | %s',
+  ['twt_help'] = 'poslji twita',
+  ['twt_prefix'] = '^0[^4Twitter^0] (^5@%s^0)',
+  ['me_help'] = 'osebne stvari',
+  ['me_prefix'] = 'jaz | %s',
+  ['do_help'] = 'RP informacija',
+  ['do_prefix'] = 'naredi | %s',
+  ['generic_argument_name'] = 'sporocilo',
+  ['generic_argument_help'] = 'sporocilo',
+}
